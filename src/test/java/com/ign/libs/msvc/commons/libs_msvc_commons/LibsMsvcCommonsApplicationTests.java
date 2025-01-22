4893602c286230e1ee7a30ee18e07f708fa12c6e
ignjs <ignacio.jara.s@gmail.com>
2025-01-21 23:13:25 -0300

    Add initial configuration for libs-msvc-commons module
    - Enable auto-configuration excluding DataSourceAutoConfiguration
    - Centralize common code for reuse across multiple microservices
    - Improve consistency and maintainability of the project

---
+++ src/test/java/com/ign/libs/msvc/commons/libs_msvc_commons/LibsMsvcCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ign.libs.msvc.commons.libs_msvc_commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibsMsvcCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
