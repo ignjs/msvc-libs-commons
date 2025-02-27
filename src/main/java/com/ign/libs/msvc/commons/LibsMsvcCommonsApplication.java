2571b35f252b7fef416d186dda1ed440002cd887
ignjs <ignacio.jara.s@gmail.com>
2025-02-26 21:05:13 -0300

    feat: add User and Role entities

--- src/main/java/com/ign/libs/msvc/commons/LibsMsvcCommonsApplication.java
+++ src/main/java/com/ign/libs/msvc/commons/LibsMsvcCommonsApplication.java
@@ -1,6 +1,5 @@
 package com.ign.libs.msvc.commons;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
